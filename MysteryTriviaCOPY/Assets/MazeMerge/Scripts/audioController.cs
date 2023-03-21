f949cb41b744ac749d942db099a6b737f5208bbd
Jason-Marr <jrmarrone23@gmail.com>
2023-03-20 22:05:44 -0400

    more audio controls, maze

--- MysteryTriviaCOPY/Assets/MazeMerge/Scripts/audioController.cs
+++ MysteryTriviaCOPY/Assets/MazeMerge/Scripts/audioController.cs
@@ -5,19 +5,27 @@ using UnityEngine.UI;
 
 public class audioController : MonoBehaviour
 {
+    public Button MuteButton;
+    public Sprite MuteImage;
+    public Sprite FullImage;
+    public int muteState=0;
     [SerializeField] Slider volumeSlider;
-    void update()
+    void Update()
     {
        if (Input.GetKey(KeyCode.Comma))
         {
-            volumeSlider.value=volumeSlider.value-0.01f;
+            volumeSlider.value=volumeSlider.value-0.001f;
             AudioListener.volume = volumeSlider.value;
             Debug.Log("<");
         }if (Input.GetKey(KeyCode.Period))
         {
-            volumeSlider.value=volumeSlider.value+0.01f;
+            volumeSlider.value=volumeSlider.value+0.001f;
             AudioListener.volume = volumeSlider.value;
             Debug.Log(">");
+        }
+        if(muteState==1){
+             volumeSlider.value=0;
+            AudioListener.volume = volumeSlider.value;
         }
 
     }
@@ -26,6 +34,19 @@ public class audioController : MonoBehaviour
     {
        AudioListener.volume = volumeSlider.value;
     }
-
+    public void MuteToggle(){
+        if(muteState==0){
+            volumeSlider.value=0;
+            AudioListener.volume = volumeSlider.value;
+            muteState=1;
+            MuteButton.GetComponent<Image>().sprite=MuteImage;
+    }
+    else if(muteState==1){
+        volumeSlider.value=1;
+            AudioListener.volume = volumeSlider.value;
+            muteState=0;
+             MuteButton.GetComponent<Image>().sprite=FullImage;
+    }
+    }
 
 }
