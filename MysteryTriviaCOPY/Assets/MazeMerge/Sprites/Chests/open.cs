090b1288e07e79e6e37351ea76178459e74016f7
Jason-Marr <jrmarrone23@gmail.com>
2023-04-10 18:44:02 -0400

    added points animation. added prefab version of pot

--- MysteryTriviaCOPY/Assets/MazeMerge/Sprites/Chests/open.cs
+++ MysteryTriviaCOPY/Assets/MazeMerge/Sprites/Chests/open.cs
@@ -8,20 +8,11 @@ public class open : MonoBehaviour
     public GameObject thisChest;
     public Animation chestOpen;
     [SerializeField]
-    // Start is called before the first frame update
-    void Start()
-    {
-         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+   
      void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("collide");
+        
         if(collision.gameObject.tag=="Player"){
         Debug.Log("opening chest");
         chestOpen.Play();
